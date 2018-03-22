07c74ec7997f6e79701fdece39e742198fe586c0
unknown <billy0196@gmail.com>
2018-03-22 17:23:55 +0100

    change some fronts, preview image doesnt work

--- montage.php
+++ montage.php
@@ -34,7 +34,14 @@
 <!-- CORP DE LA PAGE -->
 
 <div class="filter_container">
-	<a class="filter_image"> TEST1 </a>
+	<a class="filter_image"> Filtre 1 </a>
+	<a class="filter_image"> Filtre 2 </a>
+	<a class="filter_image"> Filtre 3 </a>
+	<a class="filter_image"> Filtre 4 </a>
+	<a class="filter_image"> Filtre 5 </a>
+</div>
+<div style="text-align: center;">
+	<button class="button_filter" onclick="add_filter();" alt="Ajouter un filtre">Ajouter</button>
 </div>
 
 <div class="grid-3 has-gutter main" >
@@ -49,12 +56,14 @@
 
 
 	<div class="photos_interface">
-		<canvas id="photo"></canvas>
+		<canvas id="photo">
+			Your browser does not accept canvas
+		</canvas>
 		<div style="margin-top: 1em;">
 			<form method="post" action="">
 				<!-- A rajouter dans la balise input type file pour n'autoriser qu'un certain type de fichier -->
 				<!-- accept=".jpg, .jpeg, .png, .gif" -->
-			 	<input type="file" name="photo_upload" id="monfichier" onchange="insert_imported_image();" /> 
+			 	<input type="file" name="photo_upload" id="monfichier" onchange="preview_image();" /> 
 			 	<br/>
 
 			 <!-- TODO : assigner la variable $sessions['pic_is_valid'] pour faire apparaitre le bouton publier -->
@@ -69,10 +78,16 @@
 	</div>
 
 
-
-	<div class="">
-
+	<div style="margin-top: -33px;">
+		<label class="vos_photo">Vos photos</label>
+		<div class="grid-2-small-1 has-gutter preview_container">
 		<!-- AFFICHAGE DES PHOTOS DU USER -->
+		<div class="mini_galerie">Photo1</div>
+		<div class="mini_galerie">Photo2</div>
+		<div class="mini_galerie">Photo3</div>
+		<div class="mini_galerie">Photo4</div>
+		<div class="mini_galerie">Photo5</div>
+		<div class="mini_galerie">Photo6</div>
 		<?php 
 
 			if (isset($_SESSION['id']))
@@ -81,8 +96,6 @@
 				FROM camagru.pictures;';
 		?>
 
-
-				<div class="grid-3-small-2 has-gutter" style="overflow: scroll; max-height: 500px;">
 		<?php
 				// requete de récupération des image a afficher sur l'index
 				
@@ -103,10 +116,9 @@
 					
 				}
 		?>
-				
-
-		</div>
+			</div>
 	</div>
+
 	
 </div>
 
@@ -126,28 +138,53 @@
 			context.drawImage(video, 0, 0);
 		}
 
-		function insert_imported_image()
+		// fonction appellé a l'ajout d'une photo de l'utilisateur
+		function preview_image()
 		{
-			alert("ok");
-			var file    = document.querySelector('input[type=file]').files[0]	;
-			alert(file);
-			var reader  = new FileReader();
+			alert("test");
+			var image_url = document.getElementById("monfichier").value;
+			// alert(image_url);
+			// var reader  = new FileReader();
 
-			reader.addEventListener("load", function () {
-    			photo.src = reader.result;
-    		}, false);
+			// reader.addEventListener("load", function () {
+   //  			photo.src = reader.result;
+   //  		}, false);
 
-  			if (file) {
-    		reader.readAsDataURL(file);
-  		}
+  	// 		if (file) {
+   //  		reader.readAsDataURL(file);
+  		// }
+			image = new Image();
+			
 
-			// alert("ok");
-			// image = new Image();
-			// image.src = image_imported;
+			photo.width = video.videoWidth;
+			photo.height = video.videoHeight;
+			context.drawImage(image, 0, 0);
+			image.src = "https://test-ipv6.com/";
 
-			// photo.width = video.videoWidth;
-			// photo.height = video.videoHeight;
-			// photo.drawImage(image, 0, 0);
+			var data_url = photo.toDataURL();
+			alert("url : " + data_url);
+
+
+
+      // var Pic=document.getElementById("myCanvas").toDataURL("image/jpeg");  
+  //  Pic = Pic.replace(/^data:image\/(png|jpg);base64,/, "");
+  // Pic = Pic.replace("data:image/jpeg;base64,", "");
+ 
+    // Sending the image data to Server
+   //   $.ajax({
+   //      type: 'POST',
+   //      url: 'Save_Picture.aspx/UploadPic',
+   // //     data: '{ "imageData" : "' + Pic + '" }',
+   //      data: '{ "imageData" : "' + Pic + '", "chemin" : "'+ch+'" }',
+   //      contentType: 'application/json; charset=utf-8',
+   //      dataType: 'json',
+   //      success: function (msg) {
+   //          alert("Dessin enregistre");
+   //      },
+   //      error: function (msg) {
+   //          alert("Dessin non enregistre");
+   //      }
+
 		}
 
 
